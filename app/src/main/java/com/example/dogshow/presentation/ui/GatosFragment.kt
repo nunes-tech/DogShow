01df4a039e8c057a85c9d888ac0b7be209a10128
Francisco Nunes <92335032+nunes-tech@users.noreply.github.com>
2023-08-01 13:49:33 -0300

    Comit funcional

--- app/src/main/java/com/example/dogshow/presentation/ui/GatosFragment.kt
+++ app/src/main/java/com/example/dogshow/presentation/ui/GatosFragment.kt
@@ -12,15 +12,16 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogshow.adapter.PetsAdapter
 import com.example.dogshow.databinding.FragmentGatosBinding
-import com.example.dogshow.presentation.viewmodel.MainViewModel
+import com.example.dogshow.presentation.viewmodel.CaesViewModel
+import com.example.dogshow.presentation.viewmodel.GatosViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class GatosFragment : Fragment() {
 
-    private var adapter: PetsAdapter? = null
-    private lateinit var mainViewModel: MainViewModel
+    @Inject lateinit var adapter: PetsAdapter
+    lateinit var gatosViewModel: GatosViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,16 +29,15 @@ class GatosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        adapter = PetsAdapter()
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        gatosViewModel = ViewModelProvider(this)[GatosViewModel::class.java]
         val binding = FragmentGatosBinding.inflate( inflater, container, false)
 
         binding.rvGatos.adapter = adapter
         binding.rvGatos.layoutManager = GridLayoutManager(container?.context, 2)
-        mainViewModel.recuperarImagensCats(10)
+        gatosViewModel.recuperarImagensCats(10)
 
-        mainViewModel.listaImagensCats.observe(viewLifecycleOwner) { listImagesPet ->
-            adapter?.atualizarListaImagensPet(listImagesPet)
+        gatosViewModel.listaImagensCats.observe(viewLifecycleOwner) { listImagesPet ->
+            adapter.atualizarListaImagensPet( listImagesPet )
         }
 
         binding.rvGatos.addOnScrollListener(
@@ -47,7 +47,7 @@ class GatosFragment : Fragment() {
                         val ePossivelRolarParaBaixo = recyclerView.canScrollVertically(1)
 
                         if (!ePossivelRolarParaBaixo) {
-                            mainViewModel.recuperarImagensCats(10)
+                            gatosViewModel.recuperarImagensCats(10)
                             Toast.makeText(
                                 container?.context,
                                 "Carregando mais imagens...",
@@ -60,8 +60,4 @@ class GatosFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        adapter = null
-    }
 }
--- app/src/main/java/com/example/dogshow/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/dogshow/presentation/ui/MainActivity.kt
@@ -1,28 +1,23 @@
 package com.example.dogshow.presentation.ui
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.dogshow.R
 import com.example.dogshow.databinding.ActivityMainBinding
-import com.example.dogshow.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainViewModel: MainViewModel
     private val binding by lazy {
         ActivityMainBinding.inflate( layoutInflater )
     }
 
-    @SuppressLint("ResourceType")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -30,8 +25,6 @@ class MainActivity : AppCompatActivity() {
         abrirFragment( CaesFragment() )
 
         inicializarMenu()
-
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
     }
 
